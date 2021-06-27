0c0884d2470865f2c24300fc08d089fc7cd50151
DESKTOP-FICELG4\knigh <dogukanacet@gmail.com>
2021-06-27 08:27:04 +0300

    Migration to React Redux started.

---
+++ src/store/actions.js
@@ -0,0 +1,2 @@
+export const FETCH_MOVIES = "FETCH_MOVIES";
+export const SEARCH_MOVIES = "SEARCH_MOVIES";
---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  movies: null,
+  error: false,
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
